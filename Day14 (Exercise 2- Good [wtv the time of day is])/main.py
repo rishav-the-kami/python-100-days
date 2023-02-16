355648a2b7a3f4b20062f7c0366e579d88ca6a7b
Kami Rishav <105970051+rishav-the-kami@users.noreply.github.com>
2023-02-16 14:41:18 +0530

    Update main.py

--- Day14 (Exercise 2- Good [wtv the time of day is])/main.py
+++ Day14 (Exercise 2- Good [wtv the time of day is])/main.py
@@ -4,11 +4,11 @@ hour = time.localtime().tm_hour
 
 print(time.strftime("%H:%M"))
 
-if(hour >= 0 and hour < 12):
+if hour >= 0 and hour < 12:
     print("Good Morning")
-elif(hour >= 12 and hour <= 16):
+elif hour >= 12 and hour <= 16:
     print("Good Afternoon")
-elif(hour >= 17 and hour <= 19):
+elif hour >= 17 and hour <= 19:
     print("Good Evening")
 else:
     print("Good Night")
